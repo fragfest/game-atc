55d805d33f80297afb55cfd965a599d9d42b317d
cadde <carl.adde@gmail.com>
2021-06-25 18:39:37 -0600

    basic working canvas

---
+++ src/components/Canvas.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <h1>{{ title }}</h1>
+    <canvas
+      ref="canvas"
+      class="canvas"
+      :width="width"
+      :height="height"
+    ></canvas>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Canvas',
+  props: {
+  },
+
+  data() {
+    return {
+      title: 'Canvas Component',
+      width: 800,
+      height: 600,
+    };
+  },
+
+  mounted() {
+    const canvasEl = this.$refs.canvas;
+    const ctx = canvasEl.getContext('2d');
+
+    ctx.fillRect(50,50,100,100);
+  }
+}
+</script>
+
+<style scoped>
+  .canvas {
+    border: solid 1px
+  }
+</style>
