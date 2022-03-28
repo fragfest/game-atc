65b6e6e91acb2a85951063b69742b515fc823119
cadde <carl.adde@gmail.com>
2022-03-27 21:55:30 -0600

    format code: Canvas.vue

--- src/components/Canvas.vue
+++ src/components/Canvas.vue
@@ -1,14 +1,10 @@
 <template>
   <div class="container">
-
     <!-- row-top -->
     <div class="row-top">
       <!-- scope -->
       <div class="scope">
-        <div
-          class="entity-div layer-six"
-          :style="styleFullSize"
-        />
+        <div class="entity-div layer-six" :style="styleFullSize" />
         <!-- <canvas
           ref="layerFive"
           class="canvas layer-five"
@@ -45,16 +41,14 @@
           :width="width"
           :height="height"
         ></canvas>
-        <img src="/img/london.png" :height="height">
+        <img src="/img/london.png" :height="height" />
       </div>
       <!-- end scope -->
       <!-- panel-right -->
       <div class="panel-right">
         <ul>
           <li v-for="(plane, index) in planes" :key="index">
-            <FlightStrip
-              :title="plane.title"
-            ></FlightStrip>
+            <FlightStrip :title="plane.title"></FlightStrip>
           </li>
         </ul>
       </div>
@@ -72,86 +66,133 @@
         <button @click="btnClick('right')" :style="buttonRight">Right</button>
       </div>
       <div :style="panelBottomRightStyle">
-        <p>{{ square ? square.title : '' }}</p>
+        <p>{{ square ? square.title : "" }}</p>
         <div :style="rowBelow">
-          <label for="inputHeading">Heading <small>(3 digits)</small> &nbsp;</label>
-          <input id="inputHeading" type="text" @keydown.enter="inputHeadingKeyDown" v-model="inputHeading" maxlength="3" class="input-heading">
+          <label for="inputHeading"
+            >Heading <small>(3 digits)</small> &nbsp;</label
+          >
+          <input
+            id="inputHeading"
+            type="text"
+            @keydown.enter="inputHeadingKeyDown"
+            v-model="inputHeading"
+            maxlength="3"
+            class="input-heading"
+          />
         </div>
         <div :style="rowBelow">
-          <label for="inputAltitude">Altitude <small>(3-5 digits)</small> &nbsp;</label>
-          <input id="inputAltitude" type="text" @keydown.enter="inputAltitudeKeyDown" v-model="inputAltitude" maxlength="5" class="input-heading">
+          <label for="inputAltitude"
+            >Altitude <small>(3-5 digits)</small> &nbsp;</label
+          >
+          <input
+            id="inputAltitude"
+            type="text"
+            @keydown.enter="inputAltitudeKeyDown"
+            v-model="inputAltitude"
+            maxlength="5"
+            class="input-heading"
+          />
         </div>
         <div :style="rowBelow">
           <label for="inputSpeed">Speed <small>(3 digits)</small> &nbsp;</label>
-          <input id="inputSpeed" type="text" @keydown.enter="inputSpeedKeyDown" v-model="inputSpeed" maxlength="3" class="input-heading">
+          <input
+            id="inputSpeed"
+            type="text"
+            @keydown.enter="inputSpeedKeyDown"
+            v-model="inputSpeed"
+            maxlength="3"
+            class="input-heading"
+          />
         </div>
       </div>
     </div>
     <!-- end row-bottom -->
-
   </div>
 </template>
 
 <script>
-import { setup } from '../js/game';
-import FlightStrip from './FlightStrip';
+import { setup } from "../js/game";
+import FlightStrip from "./FlightStrip";
 
 const width = 993;
 const height = 600;
 
 export default {
-  name: 'Canvas',
+  name: "Canvas",
   components: { FlightStrip },
   props: {},
 
   data() {
     return {
       planes: [],
-      inputAltitude: '',
-      inputHeading: '',
-      inputSpeed: '',
+      inputAltitude: "",
+      inputHeading: "",
+      inputSpeed: "",
       width,
       height,
       square: null,
     };
   },
   computed: {
-    styleFullSize: () => ({ width: width + 'px', height: height + 'px' }),
+    styleFullSize: () => ({ width: width + "px", height: height + "px" }),
 
-    rowBelow: () => ({ position: 'relative', 'margin-top': '10px' }),
+    rowBelow: () => ({ position: "relative", "margin-top": "10px" }),
 
-    panelBottomRightStyle: () => ({ position: 'absolute', left: '250px', top: height + 20 + 'px' }),
-    panelBottomStyle: () => ({ position: 'absolute', left: '100px', top: height + 30 + 'px' }),
-    buttonLand: () => ({ position: 'absolute', width: '120px', top: '0px' }),
-    buttonLeft: () => ({ position: 'absolute', width: '50px', top: '70px' }),
-    buttonTop: () => ({ position: 'absolute', width: '50px', left: '30px', top: '40px' }),
-    buttonBottom: () => ({ position: 'absolute', width: '50px', left: '30px', top: '100px' }),
-    buttonRight: () => ({ position: 'absolute', width: '50px', left: '55px', top: '70px' }),
+    panelBottomRightStyle: () => ({
+      position: "absolute",
+      left: "250px",
+      top: height + 20 + "px",
+    }),
+    panelBottomStyle: () => ({
+      position: "absolute",
+      left: "100px",
+      top: height + 30 + "px",
+    }),
+    buttonLand: () => ({ position: "absolute", width: "120px", top: "0px" }),
+    buttonLeft: () => ({ position: "absolute", width: "50px", top: "70px" }),
+    buttonTop: () => ({
+      position: "absolute",
+      width: "50px",
+      left: "30px",
+      top: "40px",
+    }),
+    buttonBottom: () => ({
+      position: "absolute",
+      width: "50px",
+      left: "30px",
+      top: "100px",
+    }),
+    buttonRight: () => ({
+      position: "absolute",
+      width: "50px",
+      left: "55px",
+      top: "70px",
+    }),
   },
   methods: {
-    btnClick: function(direction) {
-      if(!this.square) return;
-      if(direction === 'land') this.square.setLanding(true);
+    btnClick: function (direction) {
+      if (!this.square) return;
+      if (direction === "land") this.square.setLanding(true);
       else this.square.setHeadingStr(direction);
     },
-    inputHeadingKeyDown: function() {
-      if(!this.square) return;
-      if(Number(this.inputHeading) < 0) return
+    inputHeadingKeyDown: function () {
+      if (!this.square) return;
+      if (Number(this.inputHeading) < 0) return;
       this.square.setHeadingDegrees(this.inputHeading);
-      this.inputHeading = '';
+      this.inputHeading = "";
     },
-    inputAltitudeKeyDown: function() {
-      if(!this.square) return;
-      if(Number(this.inputAltitude) < 0) return
+    inputAltitudeKeyDown: function () {
+      if (!this.square) return;
+      if (Number(this.inputAltitude) < 0) return;
       this.square.setAltitude(this.inputAltitude, false);
-      this.inputAltitude = '';
+      this.inputAltitude = "";
     },
-    inputSpeedKeyDown: function() {
-      if(!this.square) return;
-      if(Number(this.inputSpeed) < 0) return
+    inputSpeedKeyDown: function () {
+      if (!this.square) return;
+      if (Number(this.inputSpeed) < 0) return;
       this.square.setSpeed(this.inputSpeed, false, false);
-      this.inputSpeed = '';
-    }
+      this.inputSpeed = "";
+    },
   },
 
   mounted() {
@@ -161,25 +202,38 @@ export default {
     const layerThree = this.$refs.layerThree;
     const layerFour = this.$refs.layerFour;
     // const layerFive = this.$refs.layerFive;
-    const backgroundCtx = background.getContext('2d');
-    const layerOneCtx = layerOne.getContext('2d');
-    const layerTwoCtx = layerTwo.getContext('2d');
-    const layerThreeCtx = layerThree.getContext('2d');
-    const layerFourCtx = layerFour.getContext('2d');
+    const backgroundCtx = background.getContext("2d");
+    const layerOneCtx = layerOne.getContext("2d");
+    const layerTwoCtx = layerTwo.getContext("2d");
+    const layerThreeCtx = layerThree.getContext("2d");
+    const layerFourCtx = layerFour.getContext("2d");
     // const layerFiveCtx = layerFive.getContext('2d');
-    const layerSixDiv = document.querySelector('.entity-div')
+    const layerSixDiv = document.querySelector(".entity-div");
 
     // backgroundCtx.fillStyle = 'black';
     // backgroundCtx.fillRect(0, 0, this.width, this.height);
 
     const backgroundObj = { ctx: backgroundCtx };
-    const layerTwoObj = { ctx: layerTwoCtx, width: this.width, height: this.height };
-    const layerThreeObj = { ctx: layerThreeCtx, width: this.width, height: this.height };
-    const layerFourObj = { ctx: layerFourCtx, width: this.width, height: this.height };
+    const layerTwoObj = {
+      ctx: layerTwoCtx,
+      width: this.width,
+      height: this.height,
+    };
+    const layerThreeObj = {
+      ctx: layerThreeCtx,
+      width: this.width,
+      height: this.height,
+    };
+    const layerFourObj = {
+      ctx: layerFourCtx,
+      width: this.width,
+      height: this.height,
+    };
 
-    const squareClickEventCB = squareObj => this.square = squareObj;
+    const squareClickEventCB = (squareObj) => (this.square = squareObj);
     const setupObj = setup({
-      width, height,
+      width,
+      height,
       backgroundObj,
       imgLayerObj: { ctx: layerOneCtx },
       entityLayerObj: layerTwoObj,
@@ -189,68 +243,80 @@ export default {
       squareClickEventCB,
     });
     this.planes = setupObj.planes;
-
-  } // end mounted
-}
+  }, // end mounted
+};
 </script>
 
 <style scoped lang="scss">
-  .container {
-    display: flex;
+.container {
+  display: flex;
+}
+
+.row-top {
+  display: flex;
+}
+
+.scope {
+}
+
+.panel-right {
+  margin: 0px 10px;
+  & ul {
+    list-style-type: none;
+    padding: 0 0;
   }
+}
 
-  .row-top {
-    display: flex;
-  }
+.row-bottom {
+  display: flex;
+}
 
-  .scope {
+.layer-six {
+  z-index: 6;
+}
+.layer-five {
+  z-index: 5;
+}
+.layer-four {
+  z-index: 4;
+}
+.layer-three {
+  z-index: 3;
+}
+.layer-two {
+  z-index: 2;
+}
+.layer-one {
+  z-index: 1;
+}
+.layer-zero {
+  z-index: 0;
+}
 
-  }
+.background {
+  opacity: 60%;
+  background-color: grey;
+}
 
-  .panel-right {
-    margin: 0px 10px;
-    & ul {
-      list-style-type: none;
-      padding: 0 0;
-    }
-  }
-
-  .row-bottom {
-    display: flex;
-  }
-
-  .layer-six { z-index: 6; }
-  .layer-five { z-index: 5; }
-  .layer-four { z-index: 4; }
-  .layer-three { z-index: 3; }
-  .layer-two { z-index: 2; }
-  .layer-one { z-index: 1; }
-  .layer-zero { z-index: 0; }
-
-  .background {
-    opacity: 60%;
-    background-color: grey;
-  }
-
-  .entity-div {
-    position: absolute;
-    /* width: 800px;
+.entity-div {
+  position: absolute;
+  /* width: 800px;
     height: 600px;  */
-  }
+}
 
-  .canvas {
-    border: solid 1px;
-    position: absolute;
-  }
+.canvas {
+  border: solid 1px;
+  position: absolute;
+}
 
-  p {
-    margin: 6px;
-  }
+p {
+  margin: 6px;
+}
 
-  input {
-    border: solid 1px;
-    border-radius: 3px;
-    height: 20px;
-    width: 50px;
-  }
+input {
+  border: solid 1px;
+  border-radius: 3px;
+  height: 20px;
+  width: 50px;
+}
 </style>
