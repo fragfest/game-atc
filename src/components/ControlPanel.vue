972b13c251e04e39af59144020cc08dbe6538c7c
cadde <carl.adde@gmail.com>
2022-04-12 06:03:43 -0600

    working heading field

--- src/components/ControlPanel.vue
+++ src/components/ControlPanel.vue
@@ -1,37 +1,39 @@
 <template>
   <div class="circle-panel">
     <div class="circle-inputs">
-      <button class="land" @click="btnClick('land')">Land</button>
-      <label for="inputHeading">Heading <small>(3 digits)</small> &nbsp;</label>
+      <!-- <button class="land" @click="btnClick('land')">Land</button> -->
+
+      <label class="heading-label" for="inputHeading">heading</label>
       <input
         id="inputHeading"
-        class="input-heading"
+        class="heading"
         type="text"
         @keydown.enter="inputHeadingKeyDown"
+        @input="inputEventHeading"
         v-model="inputHeading"
-        maxlength="3"
       />
-      <label for="inputAltitude"
-        >Altitude <small>(3-5 digits)</small> &nbsp;</label
-      >
+
+      <!-- <label class="heading-label" for="inputAltitude">Altitude</label>
       <input
         id="inputAltitude"
+        class="altitude"
         type="text"
         @keydown.enter="inputAltitudeKeyDown"
         v-model="inputAltitude"
         maxlength="5"
-        class="input-heading"
       />
-      <label for="inputSpeed">Speed <small>(3 digits)</small> &nbsp;</label>
+
+      <label class="heading-label" for="inputSpeed">Speed</label>
       <input
         id="inputSpeed"
+        class="speed"
         type="text"
         @keydown.enter="inputSpeedKeyDown"
         v-model="inputSpeed"
         maxlength="3"
-        class="input-heading"
-      />
+      /> -->
     </div>
+
     <div class="circle-div">
       <svg viewBox="0 0 100 100">
         <defs>
@@ -47,6 +49,8 @@
 </template>
 
 <script>
+const { isValidHeading } = require("../js/utils");
+
 export default {
   name: "ControlPanel",
   props: {
@@ -68,12 +72,38 @@ export default {
       if (direction === "land") this.planeSelected.setLanding(true);
       else this.planeSelected.setHeadingStr(direction);
     },
+
+    inputEventHeading: function (ev) {
+      const value = ev.target.value;
+      if (!this.planeSelected.setHeadingDegrees) {
+        this.inputHeading = "";
+        return;
+      }
+
+      if (value.substring(0, 3) === "---") {
+        this.inputHeading = value[3] || "";
+      }
+      if (this.inputHeading.length > 3) {
+        this.inputHeading = value.substring(0, 3);
+      }
+
+      if (!isValidHeading(this.inputHeading)) {
+        this.inputHeading = "---";
+        return;
+      }
+    },
+
     inputHeadingKeyDown: function () {
       if (!this.planeSelected.setHeadingDegrees) return;
-      if (Number(this.inputHeading) < 0) return;
+      if (this.inputHeading.length !== 3) return;
+      if (!isValidHeading(this.inputHeading)) {
+        return (this.inputHeading = "");
+      }
+      console.log("inputHeadingKeyDown", this.inputHeading);
       this.planeSelected.setHeadingDegrees(this.inputHeading);
       this.inputHeading = "";
     },
+
     inputAltitudeKeyDown: function () {
       if (!this.planeSelected.setAltitude) return;
       if (Number(this.inputAltitude) < 0) return;
@@ -101,7 +131,34 @@ export default {
   position: relative;
   display: flex;
   flex-direction: column;
-  left: 26%;
+  left: 19.2%;
+  padding-top: 50px;
+
+  .heading-label {
+    margin-bottom: 4px;
+    font-size: 14px;
+    font-family: sans-serif;
+    color: white;
+  }
+
+  :focus-visible {
+    outline-style: none;
+  }
+
+  .heading {
+    width: 36px;
+    margin-left: 5px;
+    border-radius: 4px;
+    border-style: none;
+    font-size: 20px;
+    font-family: sans-serif;
+  }
+  .speed {
+    width: 25px;
+  }
+  .altitude {
+    width: 40px;
+  }
 
   .land {
     color: green;
--- src/js/utils.js
+++ src/js/utils.js
@@ -1,4 +1,4 @@
-export const convertToPosRad = rad => (rad >= 0) ? rad : (2 * Math.PI + rad); 
+export const convertToPosRad = rad => (rad >= 0) ? rad : (2 * Math.PI + rad);
 export const convertToSmallRad = rad => (rad < 2 * Math.PI) ? rad : (rad - 2 * Math.PI);
 export const convertToSmallDegrees = degrees => (degrees <= 360) ? degrees : (degrees - 360);
 
@@ -7,3 +7,14 @@ export const radToDegrees = rad => (Number(rad) * 180 / Math.PI) + 90;
 export const degreesToRad = degrees => (Number(degrees) - 90) * Math.PI / 180;
 export const inputHeadingToRad = heading => convertToPosRad(degreesToRad(heading));
 
+export const isValidHeading = str => {
+  if (str.length > 3) return false;
+
+  const strArr = str.split('');
+  const intOnlyArr = strArr.map(str => parseInt(str))
+    .filter(int => !Number.isNaN(int));
+  const hasOnlyInts = intOnlyArr.length === str.length;
+  const isInRange = parseInt(str) >= 0 && parseInt(str) <= 360;
+  const isNotAllZeros = str !== '000';
+  return hasOnlyInts && isInRange && isNotAllZeros;
+}
